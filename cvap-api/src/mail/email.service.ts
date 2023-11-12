22ccc3755f1f5c7f4bcd4521401d0fe2b012eead
PorfirioR <porfirioperezgon@gmail.com>
2023-11-12 17:41:10 -0300

    email

--- cvap-api/src/mail/email.service.ts
+++ cvap-api/src/mail/email.service.ts
@@ -1,40 +1,37 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 import { EmailRequest } from '../models/email-request';
-import { ConfigService } from '@nestjs/config';
-import * as SendGrid from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly configService: ConfigService) {
-    const key = this.configService.get<string>('SEND_GRID_KEY') || '';
-    SendGrid.setApiKey(key);
-  }
+  constructor(private mailerService: MailerService) { }
 
   public sendEmail(request: EmailRequest): void {
-    const email = {
-        to: request.email, // list of receivers
-        from: '', // sender address
-        subject: 'Testing ✔', // Subject line
-        text: 'welcome', // plaintext body
-        html: './templates/email-template.hbs', // HTML body content
-        context: {
-          email: request.email,
-          description: request.description,
-        },
-      }
-    SendGrid.send(email)
-    // this.mailerService.sendMail({
-    //   to: request.email, // list of receivers
-    //   from: 'prezgonzlez.por@gmail.com', // sender address
-    //   subject: 'Testing Nest MailerModule ✔', // Subject line
-    //   text: 'welcome', // plaintext body
-    //   html: './templates/email-template.hbs', // HTML body content
-    //   context: {
-    //     email: request.email,
-    //     description: request.description,
-    //   },
-    // })
+    const template = `
+      <!DOCTYPE html>
+      <html lang="en">
+      
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Email Information</title>
+      </head>
+      <body>
+        <p>Hey ${request.email},</p>
+        <p>Please click below to confirm your email</p>
+        <p>
+          ${request.description}
+        </p>
+        <p>If you did not request this email you can safely ignore it.</p>
+      </body>
+      </html>
+    `;
+    this.mailerService.sendMail({
+      to: request.email,
+      subject: 'Email',
+      html: template
+    })
     .then((x) => {
       console.log(x);
     })
--- cvap-api/src/mail/mail.module.ts
+++ cvap-api/src/mail/mail.module.ts
@@ -1,38 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Global, Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 import { EmailService } from './email.service';
 
 @Global()
 @Module({
   imports: [
-    // MailerModule.forRootAsync({
-    //   imports: [ConfigModule], // import module if not enabled globally
-    //   useFactory: async (config: ConfigService) => ({
-    //     // transport: config.get("MAIL_TRANSPORT"),
-    //     // or
-    //     transport: {
-    //       host: config.get('MAIL_HOST'),
-    //       port: config.get('PORT'),
-    //       ignoreTLS: true,
-    //       secure: true,
-    //       auth: {
-    //         user: config.get('MAIL_USER'),
-    //         pass: config.get('MAIL_PASSWORD'),
-    //       },
-    //     },
-    //     defaults: {
-    //       from: `"No Reply" <${config.get('MAIL_FROM')}>`,
-    //     },
-    //     template: {
-    //       dir: join(__dirname, 'templates'),
-    //       adapter: new HandlebarsAdapter(),
-    //       options: {
-    //         strict: true,
-    //       },
-    //     },
-    //   }),
-    //   inject: [ConfigService],
-    // }),
+    MailerModule.forRootAsync({
+      useFactory: async () => ({
+        transport: {
+          host: 'smtp-relay.brevo.com',
+          port: 587,
+          ignoreTLS: true,
+          secure: false,
+          auth: {
+            user: '',
+            pass: '',
+          },
+        },
+        defaults: {
+          from: '',
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   providers: [EmailService],
   exports: [EmailService],
